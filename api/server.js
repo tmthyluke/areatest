90f1ab15158c0ee14639b4539d29dcd38ad0d9f1
Timothy Luke <timothyluke@timothys-mbp.mynetworksettings.com>
2025-06-03 13:44:38 -0400

    Fix image paths - remove /projects/ prefix to match static directory structure

--- api/server.js
+++ api/server.js
@@ -145,31 +145,31 @@ function handlePostProjectSettings(req, res, projectId) {
 
 function handleGetProjectImages(res, projectId) {
   try {
-    // Return static image paths instead of reading from filesystem
+    // Return static image paths that match the actual static directory structure
     const imageData = {
       "apple-branding": [
-        "/projects/apple-branding/images/1-apple.png",
-        "/projects/apple-branding/images/2-1-apple-alt-1.png",
-        "/projects/apple-branding/images/2-2-apple-alt-2.png",
-        "/projects/apple-branding/images/3-1-starnote.svg",
-        "/projects/apple-branding/images/3-2-apple-starnote.svg",
-        "/projects/apple-branding/images/5-apple-computer-music.png",
-        "/projects/apple-branding/images/6-apple-lettering.png",
-        "/projects/apple-branding/images/7-apple-sleeve.png"
+        "/apple-branding/images/1-apple.png",
+        "/apple-branding/images/2-1-apple-alt-1.png",
+        "/apple-branding/images/2-2-apple-alt-2.png",
+        "/apple-branding/images/3-1-starnote.svg",
+        "/apple-branding/images/3-2-apple-starnote.svg",
+        "/apple-branding/images/5-apple-computer-music.png",
+        "/apple-branding/images/6-apple-lettering.png",
+        "/apple-branding/images/7-apple-sleeve.png"
       ],
       "portfolio-2024": [
-        "/projects/portfolio-2024/images/19-starnote-necklace.png",
-        "/projects/portfolio-2024/images/22-apple-planet-day.png",
-        "/projects/portfolio-2024/images/30-xxoplex-artwork.png",
-        "/projects/portfolio-2024/images/KazooSkate3.png",
-        "/projects/portfolio-2024/images/stack.png"
+        "/portfolio-2024/images/19-starnote-necklace.png",
+        "/portfolio-2024/images/22-apple-planet-day.png",
+        "/portfolio-2024/images/30-xxoplex-artwork.png",
+        "/portfolio-2024/images/KazooSkate3.png",
+        "/portfolio-2024/images/stack.png"
       ],
       "sky-project": [
-        "/projects/sky-project/images/1-apple.png",
-        "/projects/sky-project/images/14-lifeline.png",
-        "/projects/sky-project/images/16.png",
-        "/projects/sky-project/images/Frame3203.png",
-        "/projects/sky-project/images/ShieldsPink.png"
+        "/sky-project/images/1-apple.png",
+        "/sky-project/images/14-lifeline.png",
+        "/sky-project/images/16.png",
+        "/sky-project/images/Frame3203.png",
+        "/sky-project/images/ShieldsPink.png"
       ]
     };
     
